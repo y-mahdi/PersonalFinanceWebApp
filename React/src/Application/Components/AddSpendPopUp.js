cca686214bf81925f17062d183eb5e3cdfe925b3
Youssef Mahdi <youssefmahdi.edu@gmail.com>
2022-12-25 21:17:56 +0100

    build add popup frontend
    input
    select
    buttons
    and Style

--- React/src/Application/Components/AddSpendPopUp.js
+++ React/src/Application/Components/AddSpendPopUp.js
@@ -3,11 +3,26 @@
 export default function AddSpendPopUp(onOpen,onClose) {
     if(onOpen){
         return(<div className="addspend-pop-up">
-            <div className="popup-add-title">Add a Spent</div>
-            <input type={'number'} placeholder='Amount' />
-        </div>)
+                    <div className="popup-add-title">Add a Spent</div>
+                    <input min={1} type={'number'} placeholder='Amount' /><br />
+                    <select>
+                        <option value={'food'}>Food</option>
+                        <option value={'entertainement'}>Entertainement</option>
+                        <option value={'housing'}>Housing</option>
+                        <option value={'education'}>Education</option>
+                        <option value={'travel'}>Travel</option>
+                        <option value={'transport'}>Transport</option>
+                        <option value={'others'}>Others</option>
+                    </select><br/>
+                    <button>Add</button><br />
+                    <button onClick={()=>{
+                        onClose()
+                    }}>Cancel</button>
+                 </div>)
     }
-    return(
-        <div></div>
-    )
+    else{
+        return(
+            <div></div>
+        )
+    }
 }
--- React/src/Application/Components/HeaderApp.js
+++ React/src/Application/Components/HeaderApp.js
@@ -2,22 +2,27 @@ import '../Style/application.css';
 import creditcard from '../../Icons/creditcard.svg';
 import plusicon from '../../Icons/plus.svg';
 import AddSpendPopUp from './AddSpendPopUp';
-import {useState} from 'react';
+import {useState,useEffect} from 'react';
 export default function HeaderApp() {
-    const [open, setOpen] = useState(true);
+    const [open, setOpen] = useState(false);
     const closeAddPopUp=()=>{
         setOpen(false)
     }
+    useEffect(()=>{
+        console.log(open)
+    },[open])
     return (
         <div>
             <div className="logo-panel-app">
                 <img src={creditcard} alt="credit card" width="50px" />
             </div>
-            <div className="btn-add-spent">
+            <button className="btn-add-spent" onClick={()=>{
+                setOpen(true)
+            }}>
                 <img src={plusicon} alt="plus icon" width="20px"/>
                 Add a Spend
-            </div>
-            <AddSpendPopUp onOpen={open} onClose={closeAddPopUp} />
+            </button>
+            {AddSpendPopUp(open,closeAddPopUp)}
         </div>
     )
-}
+}   
