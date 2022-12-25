11514963cd6bb0c8d61d5740ab5b74692bcac78f
Youssef Mahdi <youssefmahdi.edu@gmail.com>
2022-12-25 18:15:57 +0100

    Application.js init

---
+++ React/src/Application/Application.js
@@ -0,0 +1,10 @@
+
+
+
+export default function Application() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
--- React/src/Routes/Routes.js
+++ React/src/Routes/Routes.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router,Routes,Route} from "react-router-dom";
 import Login from "../Login/login";
 import Home from "../HomePage/Home";
 import Signup from "../Signup/Signup";
+import Application from "../Application/Application";
 export default function RoutesApp() {
     return(
         <>
@@ -10,6 +11,7 @@ export default function RoutesApp() {
                     <Route path="/" element={<Home />} />
                     <Route path="/Auth" element={<Login />} />
                     <Route path="/SignIn" element={<Signup />} />
+                    <Route path="/Application" element={<Application />} />
                 </Routes>
             </Router>
         </>
